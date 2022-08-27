bd835aee5400c3572021095c95863a3c9f357dfc
momolokii-dev <didy1234567@gmail.com>
2022-08-27 15:39:15 +0200

    axios interceptors

--- src/utils/fetchJson.js
+++ src/utils/fetchJson.js
@@ -1,8 +1,16 @@
 import axios from "axios"
-
+import { getLocalStorage } from "./LocalStorage"
 
 const fetchJson = axios.create({
     baseURL: "https://mo-jobs-api-v1.herokuapp.com/api/v1",
+})
+
+fetchJson.interceptors.request.use(config => {
+    const user = getLocalStorage()
+    if (user) {
+        config.headers.common["Authorization"] = `Bearer ${ user.token }`
+    }
+    return config
 })
 export default fetchJson
 
