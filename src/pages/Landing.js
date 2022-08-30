23a07a41b1f2e91115a3b34169fce8fe239b3804
momolokii-dev <didy1234567@gmail.com>
2022-08-31 01:49:20 +0200

    landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,36 +1,32 @@
 import React from "react";
 import { LogoIcon } from "../assets";
 import { Link } from "react-router-dom"
+import heroImg from "../assets/hero.jpg"
 
 const Landing = () => {
     return (
         <>
-            <section>
-                <header className=" bg-white ">
-                    <nav className="2xl:container 2xl:mx-auto sm:py-6 sm:px-7 py-5 px-4">
-                        <div className="flex justify-between items-center">
-                            <div className="flex items-center flex-shrink-0 px-4">
-                                <LogoIcon classes="w-32" />
-                            </div>
-                            <div className="flex gap-4">
-                                <Link to="/register" className="rounded-md tracking-wide bg-gradient-to-r border border-indigo-600 text-indigo-700 px-4 py-2 font-semibold">Sign Up</Link>
-                                <Link to="/login" className="rounded-md tracking-wide bg-gradient-to-r from-blue-600  to-indigo-400 text-white  px-4 py-2 font-semibold">Sign In</Link>
-                            </div>
+            <section className="container mx-auto">
+                <header >
+                    <nav className="px-5 py-3">
+                        <div className="flex items-center flex-shrink-0 px-4">
+                            <LogoIcon classes="w-32 xl:w-52" />
                         </div>
                     </nav>
                 </header>
 
-                <div className="relative">
-                    <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
-                    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                        <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
+                {/* hero */}
+                <main className="relative p-4">
+                    <div className="absolute inset-x-0 bottom-0 h-1/2 " />
+                    <div className=" mx-auto sm:px-6 lg:px-8">
+                        <div className="relative shadow-xl rounded-2xl sm:overflow-hidden">
                             <div className="absolute inset-0">
                                 <img
                                     className="h-full w-full object-cover"
-                                    src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
-                                    alt="People working on laptops"
+                                    src={heroImg}
+                                    alt="hero img"
                                 />
-                                <div className="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply" />
+                                <div className="absolute inset-0 bg-gradient-to-r from-purple-700 to-indigo-700 mix-blend-multiply" />
                             </div>
                             <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
                                 <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
@@ -43,24 +39,22 @@ const Landing = () => {
                                 </p>
                                 <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
                                     <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
-                                        <button
-                                            href="#"
-                                            className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"
+                                        <Link to="/register"
+                                            className="flex items-center justify-center px-4 py-3 border border-transparent w-full text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"
                                         >
-                                            Get started
-                                        </button>
-                                        <button
-                                            href="#"
-                                            className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8"
+                                            Sign up
+                                        </Link>
+                                        <Link to="/login"
+                                            className="flex items-center justify-center px-4 py-3 border border-transparent w-full text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8"
                                         >
-                                            Live demo
-                                        </button>
+                                            Sign in
+                                        </Link>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </main>
             </section>
         </>
     );
