17155bb5fd1c4b2cfcfb5841e3e3eab932da9cf7
momo-dev1 <didy1234567@gmail.com>
2024-05-19 06:30:04 +0300

    without Credentials test

--- src/utils/fetchJson.ts
+++ src/utils/fetchJson.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const fetchJson = axios.create({
     baseURL: "https://jobify-api-mlvw.onrender.com/api/v2",
-    withCredentials: true,
+    // withCredentials: true,
     headers: {
         "Content-type": "application/json",
     },
