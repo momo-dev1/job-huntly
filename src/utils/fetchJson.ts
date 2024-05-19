9a65b945d525a955fa0e15464d95469b71a7ac7a
momo-dev1 <didy1234567@gmail.com>
2024-05-19 06:39:01 +0300

    chore: fixing with credentials

--- src/utils/fetchJson.ts
+++ src/utils/fetchJson.ts
@@ -2,11 +2,10 @@ import axios from "axios"
 
 const fetchJson = axios.create({
     baseURL: "https://jobify-api-mlvw.onrender.com/api/v2",
-    // withCredentials: true,
+    withCredentials: true,
     headers: {
         "Content-type": "application/json",
     },
 })
 
 export default fetchJson
-
